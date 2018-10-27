bbcdcb552c210c45b2a653f553d77a6a41b04079
Jonathan Somer <jonathan.somer@fiverr.com>
2018-10-27 14:49:39 +0300

    get train and test data

--- data_utils.py
+++ data_utils.py
@@ -13,50 +13,73 @@ TRAIN_Y_DATES = 'train_y_date.txt'
 TRAIN_Y_RATINGS = 'train_y_rating.txt'
 
 NUMBER_OF_MOVIES = 99
+NUMBER_OF_USERS_TRAIN = 10000
+NUMBER_OF_USERS_TEST = 2931
+
 # X: for 1000 users, 2 rows - first is the rating of 99 movies, second is the date for each rating
 # Y: for 1000 users, rating and date for the "miss congeniality" film
 def get_X_Y_train():
-	X = []
-	Y = []
+	Y = _get_Y(train=True)
+	X = _get_X(train=True)
 
-	with open(_get_data_file_full_path_by_name(TRAIN_X_RATINGS)) as f:
-		for line in f:
-			movie_ratings = [int(rating) for rating in line.split()]
-			assert len(movie_ratings) == NUMBER_OF_MOVIES
-			X.append([movie_ratings])
+	assert X.shape == (NUMBER_OF_USERS_TRAIN, 2, NUMBER_OF_MOVIES)
+	assert Y.shape == (NUMBER_OF_USERS_TRAIN, 2)
 
-	with open(_get_data_file_full_path_by_name(TRAIN_X_DATES)) as f:
-		for line_number, line in enumerate(f):
-			rating_dates = [int(rating) for rating in line.split()]
-			assert len(rating_dates) == NUMBER_OF_MOVIES
-			X[line_number].append(rating_dates)
-
-	with open(_get_data_file_full_path_by_name(TRAIN_Y_RATINGS)) as f:
-		for line in f:
-			rating = int(line.split()[0])
-			Y.append([rating])
-
-	with open(_get_data_file_full_path_by_name(TRAIN_X_DATES)) as f:
-		for line_number, line in enumerate(f):
-			date = int(line.split()[0])
-			Y[line_number].append(date)
-
-	X = np.array(X, dtype='float')
-	Y = np.array(Y, dtype='float')
-
-	assert X.shape == (10000, 2, 99)
-	assert Y.shape == (10000, 2)
-
-
-	X[X == 0] = np.nan
-	Y[Y == 0] = np.nan
-	
 	return X, Y
 
+def get_X_Y_test():
+	Y = _get_Y(train=False)
+	X = _get_X(train=False)
 
+	assert X.shape == (NUMBER_OF_USERS_TEST, 2, NUMBER_OF_MOVIES)
+	assert Y.shape == (NUMBER_OF_USERS_TEST, 1)
 
+	return X, Y
 
 # "PRIVATE" METHODS
 
 def _get_data_file_full_path_by_name(name):
 	return os.path.join(DATA_BASE_DIR, name)
+
+def _get_X(train=True):
+	X = []
+
+	dates_file_name = TRAIN_X_DATES if train else TEST_X_DATES
+	ratings_file_name = TRAIN_X_RATINGS if train else TEST_X_RATINGS
+
+	with open(_get_data_file_full_path_by_name(dates_file_name)) as f:
+		for line in f:
+			dates = [int(date) for date in line.split()]
+			assert len(dates) == NUMBER_OF_MOVIES
+			X.append([dates])
+
+	with open(_get_data_file_full_path_by_name(ratings_file_name)) as f:
+		for line_number, line in enumerate(f):
+			ratings = [int(rating) for rating in line.split()]
+			assert len(ratings) == NUMBER_OF_MOVIES
+			X[line_number].append(ratings)
+
+	X = np.array(X, dtype='float')
+	X[X == 0] = np.nan
+
+	return X
+
+def _get_Y(train=True):
+	Y = []
+
+	dates_file_name = TRAIN_Y_DATES if train else TEST_Y_DATES
+
+	with open(_get_data_file_full_path_by_name(dates_file_name)) as f:
+		for line in f:
+			date = int(line.split()[0])
+			Y.append([date])
+
+	if train:
+		with open(_get_data_file_full_path_by_name(TRAIN_Y_RATINGS)) as f:
+			for line_number, line in enumerate(f):
+				rating = int(line.split()[0])
+				Y[line_number].append(rating)
+
+	Y = np.array(Y, dtype='float')
+	Y[Y == 0] = np.nan
+	return Y
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from data_utils import *
 
 X_train, Y_train = get_X_Y_train()
+X_test, Y_test = get_X_Y_test()
