24d1042329418c51bd3d2a783944c08cb9cbe306
Ido Kessler <t-idkess@microsoft.com>
2018-10-27 19:01:51 +0300

    Change the prediction to include only the rates

--- naive_regresor.py
+++ naive_regresor.py
@@ -1,13 +1,16 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+
 class NaiveRegresor(object):
 
     def __init__(self):
         self._reg = LinearRegression()
 
     def fit(self, X, Y):
-        self._reg = self._reg.fit(X, y)
-    
+        assert len(X.shape) == 3
+        X = X.reshape(X.shape[0], -1)
+        self._reg = self._reg.fit(X, Y)
+
     def predict(self, X):
-        return self._reg.predict(X)
+        return self._reg.predict(X.reshape(X.shape[0], -1))
